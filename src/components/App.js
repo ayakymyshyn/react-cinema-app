35333680df1d116c6c47f0114203140ec19c0cad
SuperMEgaKiller <oleg.truhan789@gmail.com>
2019-04-05 21:49:14 +0300

    First Dev Branch Commit

--- src/components/App.js
+++ src/components/App.js
@@ -1,10 +1,14 @@
 import React from "react";
 import "./App.scss";
 
-import DateNavigation from "./DateNavigation/DateNavigation";
-
 const App = () => {
-  return <DateNavigation />;
+  return (
+    <div className="main-app">
+      <div className="main-app__header">React Cinema App</div>
+      <div className="main-app__content">content</div>
+      <div className="main-app__footer">Footer</div>
+    </div>
+  );
 };
 
 export default App;
