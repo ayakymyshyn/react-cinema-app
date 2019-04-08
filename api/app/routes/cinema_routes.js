bcbb6d4a09ee776e4560d7053f60249c0bc6056c
Andrii <andrewyakymyshyn@gmail.com>
2019-04-08 13:51:18 +0300

    Added PUT, DELETE roues

--- api/app/routes/cinema_routes.js
+++ api/app/routes/cinema_routes.js
@@ -1,3 +1,5 @@
+var ObjectID = require('mongodb').ObjectID;
+
 module.exports = function(app, db) {
     app.get('/films', (req, res) => {
         db.collection('films').find({}).toArray((err, item) => {
@@ -9,6 +11,28 @@ module.exports = function(app, db) {
         db.collection('films').find(URLparams).toArray((err, item) => {
             err ?  res.send({'error':'An error has occurred'}) : res.send(item);
         })
+    });
+    app.delete('/film', (req, res) => {
+      const id = req.query;
+      const details = {'_id': new ObjectID(id.id)};
+      db.collection('films').remove(details, (err, item) => {
+        err ? res.send('Error! An error has occured!') : res.send(`Film was deleted!`);
+      })
+    });
+    app.put('/film', (req, res) => {
+      const id = req.query;
+      const details = {'_id': new ObjectID(id.id)};
+      const film = { 
+        filmTitle: req.body.filmTitle, 
+        description: req.body.description,
+        date: req.body.date,
+        trailerUrl: req.body.trailerUrl,
+        imgSrc: req.body.imgSrc,
+        ticketPrice: req.body.ticketPrice,
+      };
+      db.collection('films').update(details, film, (err, result) => {
+        err ? res.send('Error! An error has occured!') : res.send(film);
+      });
     });
     app.post('/films', (req, res) => {
       const film = { 
@@ -23,4 +47,5 @@ module.exports = function(app, db) {
         err ?  res.send({'error':'An error has occurred'}) : res.send(result.ops[0]);
       });
     });
+
   };
